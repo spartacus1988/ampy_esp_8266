670ffccbf9638e7f6aa22ee6d6ee64178ce49978
mixxxxx <spartacusxxxxxx@gmail.com>
2018-01-23 16:20:05 +0300

    add copy shell

---
+++ ampy/copy_to_esp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ampy -p /dev/ttyUSB0 put $HOME/ampy/ampy_esp_8266/ampy/boot.py  /boot.py
+ampy -p /dev/ttyUSB0 put $HOME/ampy/ampy_esp_8266/ampy/HTTPsServer.py  /HTTPsServer.py
+ampy -p /dev/ttyUSB0 put $HOME/ampy/ampy_esp_8266/ampy/connect.py  /connect.py
+ampy -p /dev/ttyUSB0 put $HOME/ampy/ampy_esp_8266/ampy/extract_credential.py  /extract_credential.py
+ampy -p /dev/ttyUSB0 put $HOME/ampy/ampy_esp_8266/ampy/credentials.txt  /credentials.txt
+
+ampy -p /dev/ttyUSB0 mkdir lib
+ampy -p /dev/ttyUSB0 mkdir /lib/uasyncio
+ampy -p /dev/ttyUSB0 put $HOME/ampy/ampy_esp_8266/ampy/micropython-uasyncio/__init__.py  /lib/uasyncio/__init__.py
+ampy -p /dev/ttyUSB0 put $HOME/ampy/ampy_esp_8266/ampy/micropython-uasyncio/core.py  /lib/uasyncio/core.py
+ampy -p /dev/ttyUSB0 put $HOME/ampy/ampy_esp_8266/ampy/micropython-uasyncio/core.mpy  /lib/uasyncio/core.mpy
+echo "All files was copied successful"
