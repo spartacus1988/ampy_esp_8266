89af913e3f7deb766584db60470a069f77b1354f
mixxxxx <spartacusxxxxxx@gmail.com>
2018-03-24 18:13:17 +0300

    fucking %s format

--- ampy/run_socket.py
+++ ampy/run_socket.py
@@ -5,33 +5,15 @@ import uasyncio as asyncio
 import network
 import urandom
 
-
-
 class serverSocketClass:
 	
 	def __init__(self):
 		self._init_ap_if()
-		#self.ap_if = network.WLAN(network.AP_IF)
-		#self.ap_if.active(True)
-		
-		# self.my_ssid = self.ap_if.config('essid')
-		# channel = urandom.getrandbits(4)
-		# if channel > 11:
-		# 	channel =  16 - channel
-		# self.my_channel = channel
-		# self.ap_if.config(essid=self.my_ssid, channel=self.my_channel)
-		# network.phy_mode(1)
-
-		#self.ap_if.config(essid='FUCK_YOU_SSID', channel=7)
-		#print(self.ap_if.config('channel'))
-		#print(self.ap_if.config('essid'))
-		#print(network.phy_mode())
-		
-
-
+		# self.ap_if = network.WLAN(network.AP_IF)
+		# self.ap_if.active(True)
 		self.addr = ('192.168.4.1', 8080)
 		self.serverSocket = socket.socket(socket.AF_INET,	socket.SOCK_STREAM)
-		#self.serverSocket.settimeout(1)
+		self.serverSocket.settimeout(1)
 		self.serverSocket.bind(self.addr)
 		self.serverSocket.listen(3)
 		#init ADC	
@@ -41,55 +23,27 @@ class serverSocketClass:
 		self.sVin = None
 		self.adc = ADC(0)
 
-
-
 	def _init_ap_if(self):
 		self.ap_if = network.WLAN(network.AP_IF)
 		self.ap_if.active(True)
-		# self.my_ssid = self.ap_if.config('essid')
-		# print(self.my_ssid)
-		# channel = urandom.getrandbits(4)
-		# if channel > 11:
-		# 	channel =  16 - channel
-		# self.my_channel = channel
-		# self.ap_if.config(essid=self.my_ssid, channel=self.my_channel)
-		# network.phy_mode(3)
-
-
+		#self.my_ssid = self.ap_if.config('essid')
+		self.my_ssid = 'MicroPython-06ab0a'
+		print(self.my_ssid)
+		channel = urandom.getrandbits(4)
+		if channel > 11:
+			channel =  16 - channel
+		self.my_channel = channel
+		self.ap_if.config(essid=self.my_ssid, channel=self.my_channel)
+		network.phy_mode(1)
 
 	def __await__(self, timeout):
-		self.serverSocket.settimeout(timeout)
+
 		#self.ap_if.active(True)
-		#self.my_ssid = self.ap_if.config('essid')
-
-		#self.ap_if.config(essid=self.my_ssid, channel=13)
-		#self.ap_if.config(essid='FUCK_YOU_SSID', channel=7)
-		#print('serverSocketClass__await__')
-
-
-		#print(self.ap_if.config('channel'))
-		#print(self.ap_if.config('essid'))
-		#print(network.phy_mode())
-
 		#await asyncio.sleep(1)
+		self.serverSocket.settimeout(timeout)
 		while True:
 			try:
 				print("loop socket")
-				#self._init_ap_if()
-				#self.ap_if.active(True)
-				# self.my_ssid = self.ap_if.config('essid')
-				# channel = urandom.getrandbits(4)
-				# if channel > 11:
-				# 	channel =  16 - channel
-				# self.my_channel = channel
-				# self.ap_if.config(essid=self.my_ssid, channel=self.my_channel)
-				# network.phy_mode(1)
-				
-				
-				print(self.ap_if.config('channel'))
-
-
-
 				cl, addr = self.serverSocket.accept()
 				cl_file	= cl.makefile('rwb', 0)
 				while	True:
@@ -101,24 +55,39 @@ class serverSocketClass:
 				self.Dn = self.adc.read()
 				self.vin = self.LSB * self.Dn
 				self.sVin = "{:.3f}".format(self.vin)
-
+				#print(self.my_ssid)
+				#d = "dsdsdsdsadsda"
+				#d = self.my_ssid
 				
+
+				# #Prepare the response
+				# response = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
+				# response += """<!DOCTYPE html>
+				# <html>
+				# <body>
+				# <h3>Voltage on pin A0 =	%sV </h3>
+				# </body>
+				# </html>\n""" % self.sVin
+
+
 
 				#Prepare the response
 				response = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n'
 				response += """<!DOCTYPE html>
 				<html>
 				<body>
+				<h3>%s=%s</h3>
 				<h3>dfsdfsdgsdgsdgsd=%s</h3>
 				</body>
-				</html>\n""" % self.my_ssid# self.sVin# 'ssid', '5.0'
+				</html>\n""" % (self.my_ssid, self.sVin, self.sVin)# self.sVin# 'ssid', '5.0'
+
 
 
 				#response = sVin
 				cl.send(response)
 				cl.close()
 			except:
-				await asyncio.sleep(timeout)
+				await asyncio.sleep(5)
 				
 
 	__iter__ = __await__ 
